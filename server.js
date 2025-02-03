1429ba97b84de0ab4d4d5e943dd10b9718cfa1d2
Tyler Rubino <tylerrubi9@gmail.com>
2025-02-03 17:18:47 -0500

    updated project with latest changes

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-import { value } from './test-named.js';
-import someValue from './test-default.js';
+import express from 'express';
+const app = express();
 
-console.log(value);
-console.log(someValue);
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.post('/', (req, res) => {
+	console.log(req);
+	res.json({ message: 'data received', data: req.body });
+});
+
+app.listen(5100, () => {
+	console.log('server running....');
+});
